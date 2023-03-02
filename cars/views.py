c03920314e099a70e33ec693004547e05c529349
dougwash <dwash56@icloud.com>
2023-03-01 21:28:02 -0500

    Update car view in python + created Car Post in postman

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import CarSerializer
 from .models import Car
 
@@ -11,4 +12,8 @@ def cars_list(request):
         serializer = CarSerializer(cars, many=True)
         return Response (serializer.data) 
     elif request.method == 'POST':
-        
+        serializer = CarSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+       
